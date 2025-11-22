5a01d588d8d10b5533ada92cdc54002b34da712a
zacariasprojeto <144869115+zacariasprojeto@users.noreply.github.com>
2025-11-22 20:02:03 -0300

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,24 @@
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[^\d]+/g, '');
+    
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+    
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+        soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    
+    let resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.charAt(9))) return false;
+    
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+        soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    
+    return resto === parseInt(cpf.charAt(10));
+}
